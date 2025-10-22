126e829e36b9c32e4610f52f206404c642fd4d00
Abel KPOKOUTA <kpokoutaabel@gmail.com>
2025-10-22 22:29:59 +0100

    Mise à jours info

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'chatbot'
 
 urlpatterns = [
-    path('', views.chatbot_view, name='chat'),
-    path('api/chat/', views.chat_api, name='chat_api'),
-    path('api/health/', views.health_check, name='health_check'),  # Health check
+    path('', views.chatbot_redirect, name='chat'),
 ]
