67aeb070ab13addbf73adca2e193dba89f71b402
Abel KPOKOUTA <kpokoutaabel@gmail.com>
2025-12-14 18:43:01 +0100

    last udapte

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #app_name='core'
 
 urlpatterns = [
@@ -11,3 +13,10 @@ urlpatterns = [
     path('politique-confidentialite/', views.politique, name='politique'),
     
 ]
+
+# Pour servir les fichiers médias en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
